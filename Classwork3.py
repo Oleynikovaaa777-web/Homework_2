4c7ecddce99954d0edabb0493dfee160f228fa6d
alinaoleynikova <oleynikovaaa777@gmail.com>
2020-09-12 13:57:13 -0700

    Add Classwork

---
+++ Classwork3.py
@@ -0,0 +1,55 @@
+#
+# # 19
+#
+# a = int(input('Enter a number'))
+# for x in range(2, a):
+#     if a%x == 0:
+#         print('It\'s not a prime number')
+#         break
+# else:
+#     print('It\'s a prime number' )
+
+# 20
+#
+# a = int(input('Enter a number'))
+# b = int(input('Enter a number'))
+#
+# while a != b:
+#     if a > b:
+#         a = a - b
+#     elif a < b:
+#         b = b - a
+# else:
+#     print(f'{a} It\'s a lowest divider')
+
+
+# 21
+
+
+# a = int(input('Enter a number: '))
+# b = int(input('Enter a number: '))
+# count = 0
+#
+# while a != 0 and b != 0:
+#     count += 1
+#     if a > b:
+#         a = a % b
+#     else:
+#         b = b % a
+# else:
+#     print(a + b)
+#     print(count)
+
+
+#
+# 23
+
+# a = int(input('Enter a number: '))
+# count = 1
+#
+# while a != 0:
+#     count += 1
+#     a = int(input('Enter a number: '))
+# else:
+#     print(count)
+
