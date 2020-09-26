432a98c91b3f379ab822f601f474390815a9a79a
alinaoleynikova <oleynikovaaa777@gmail.com>
2020-09-26 14:56:03 -0700

    Add Homework

--- HW6.py
+++ HW6.py
@@ -6,13 +6,26 @@
 # # 16
 
 #
-a = int(input('Enter a number: '))
-b = int(input('Enter a number: '))
-count = 1
+# a = int(input('Enter a number: '))
+# b = int(input('Enter a number: '))
+# count = 1
+#
+# c = range(a, b+1)
+#
+# for x in c:
+#     count = x * x
+#     print(count)
 
-c = range(a, b+1)
+########
 
-for x in c:
-    count = x * x
-    print(count)
+# from random import uniform
+# # uniform(a,b)
+#############
 
+
+# 17
+
+N = int(input('Enter a number: '))
+from random import randint
+N = randint(0, N+1)
+print(N)
