79721bb3bd9a5461fe0cc62b20314b1d02d9697c
alinaoleynikova <oleynikovaaa777@gmail.com>
2020-09-26 14:59:24 -0700

    Add Homework

--- HW6.py
+++ HW6.py
@@ -1,4 +1,4 @@
-# Homwork:
+# Homework:
 #
 # 16, 17,
 # read :page 41
@@ -9,12 +9,15 @@
 # a = int(input('Enter a number: '))
 # b = int(input('Enter a number: '))
 # count = 1
+# summa = 0
 #
 # c = range(a, b+1)
 #
 # for x in c:
 #     count = x * x
-#     print(count)
+#     # print(count)
+#     summa += count
+#     print(summa)
 
 ########
 
@@ -24,8 +27,8 @@
 
 
 # 17
-
-N = int(input('Enter a number: '))
-from random import randint
-N = randint(0, N+1)
-print(N)
+#
+# N = int(input('Enter a number: '))
+# from random import randint
+# N = randint(0, N+1)
+# print(N)
